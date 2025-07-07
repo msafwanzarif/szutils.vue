ade673e6272eec4140dfa2d0638a05f0769d735e
Safwan Zarif <msafwanzarif@gmail.com>
2025-07-07 23:35:42 +0800

    chore: add lab testing

---
+++ lab/vite/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useDuration } from 'szutils.vue';
+
+const duration = useDuration({ minutes: 1 });
+
+function add90Seconds() {
+  duration.add({ seconds: 90 });
+}
+</script>
+
+<template>
+  <h2>Duration: {{ duration.hours }}:{{ duration.minutes }}:{{ duration.seconds }}</h2>
+  <button @click="add90Seconds">+90 Seconds</button>
+</template>
---
+++ lab/vite/vite.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+
+export default defineConfig(({ command }) => ({
+  plugins: [
+    Vue(),
+  ],
+  resolve: command === 'build'
+    ? {}
+    : {
+        alias: {
+          'szutils-vue': resolve(__dirname, '../../src/index.ts'),
+        },
+      },
+  build: {
+    minify: false,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('szutils'))
+            return 'szutils'
+          else
+            return 'vendor'
+        },
+      },
+    },
+  },
+}))
