1dfbb6576a14ccab6a3064e58ae137ad812222c5
Safwan Zarif <msafwanzarif@gmail.com>
2025-07-07 23:37:09 +0800

    fix: rectify package alias

--- lab/vite/vite.config.ts
+++ lab/vite/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => ({
     ? {}
     : {
         alias: {
-          'szutils-vue': resolve(__dirname, '../../src/index.ts'),
+          'szutils.vue': resolve(__dirname, '../../src/index.ts'),
         },
       },
   build: {
