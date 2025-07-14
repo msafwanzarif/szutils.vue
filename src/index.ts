513f137c6c077d5119b11c043e449c055388c98b
Safwan Zarif <msafwanzarif@gmail.com>
2025-07-14 15:32:15 +0800

    feat: export useMetas in our package

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './useDuration';
 export * from './useDurationDisplay'
 export { useHabitTracker } from './useHabitTracker';
+export { useMetas } from './useMetas';
 export { useTimer } from './useTimer';
 export { useTimeTick } from './useTimeTick'
 export { useTimeTickShared } from './useTimeTickShared'
